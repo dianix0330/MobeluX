fe8ec97f9566393c4c1be74ec9bf7e42f224297e
dianix0330 <dianix0330@gmail.com>
2023-01-19 19:01:50 -0800

    header complete

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./index.scss";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo"></div>
+    </header>
+  );
+}
